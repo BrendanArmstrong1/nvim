6d76fe0e42aad8d93d08337844342676329bf852
Brendan Armstrong <brendanarmstrong@proton.me>
2023-01-07 11:08:23 -0500

    got rid of unused functions

--- lua/lazyvim/util.lua
+++ lua/lazyvim/util.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-M.root_patterns = { ".git", "/lua" }
-
 ---@param on_attach fun(client, buffer)
 function M.on_attach(on_attach)
   vim.api.nvim_create_autocmd("LspAttach", {
@@ -11,63 +9,6 @@ function M.on_attach(on_attach)
       on_attach(client, buffer)
     end,
   })
-end
-
--- returns the root directory based on:
--- * lsp workspace folders
--- * lsp root_dir
--- * root pattern of filename of the current buffer
--- * root pattern of cwd
-function M.get_root()
-  local path = vim.api.nvim_buf_get_name(0)
-  path = path ~= "" and vim.loop.fs_realpath(path) or nil
-  local roots = {}
-  if path then
-    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
-      local workspace = client.config.workspace_folders
-      local paths = workspace
-          and vim.tbl_map(function(ws)
-            return vim.uri_to_fname(ws.uri)
-          end, workspace)
-        or client.config.root_dir and { client.config.root_dir }
-        or {}
-      for _, p in ipairs(paths) do
-        local r = vim.loop.fs_realpath(p)
-        if path:find(r, 1, true) then
-          roots[#roots + 1] = r
-        end
-      end
-    end
-  end
-  table.sort(roots, function(a, b)
-    return #a > #b
-  end)
-  local root = roots[1]
-  if not root then
-    path = path and vim.fs.dirname(path) or vim.loop.cwd()
-    root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
-    root = root and vim.fs.dirname(root) or vim.loop.cwd()
-  end
-  return root
-end
-
-function M.telescope(builtin, opts)
-  return function()
-    require("telescope.builtin")[builtin](vim.tbl_deep_extend("force", { cwd = M.get_root() }, opts or {}))
-  end
-end
-
-function M.float_term(cmd, opts)
-  opts = vim.tbl_deep_extend("force", {
-    terminal = true,
-    close_on_exit = true,
-    enter = true,
-    float = {
-      size = { width = 0.9, height = 0.9 },
-      margin = { top = 0, right = 0, bottom = 0, left = 0 },
-    },
-  }, opts or {})
-  require("lazy.util").open_cmd(cmd, opts)
 end
 
 return M
