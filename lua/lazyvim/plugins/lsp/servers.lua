f5d647cbf6b409da72cd2ae79aaeb606918a80c2
Brendan Armstrong <brendanarmstrong@proton.me>
2023-01-07 12:56:25 -0500

    solved root problem with python servers

--- lua/lazyvim/plugins/lsp/servers.lua
+++ lua/lazyvim/plugins/lsp/servers.lua
@@ -1,3 +1,4 @@
+local util = require("lspconfig/util")
 -- Add any servers here together with their settings
 local servers = {
   bashls = {},
@@ -6,8 +7,22 @@ local servers = {
   -- tsserver = {},
   -- html = {},
   -- jsonls = {},
-  pyright = {},
-  yamlls = {},
+  pyright = {
+    root_dir = function(fname)
+      local root_files = {
+        ".git",
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "Pipfile",
+        "pyrightconfig.json",
+      }
+      return util.root_pattern(root_files.unpack)(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+    end,
+  },
+  -- yamlls = {},
+  -- selene = {},
   sumneko_lua = {
     settings = {
       Lua = {
