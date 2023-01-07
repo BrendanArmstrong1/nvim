401b44e796314bdf438351da36cce8b13e149af0
Brendan Armstrong <brendanarmstrong@proton.me>
2023-01-07 11:01:59 -0500

    deleted unused servers

--- lua/lazyvim/plugins/lsp/servers.lua
+++ lua/lazyvim/plugins/lsp/servers.lua
@@ -1,11 +1,11 @@
 -- Add any servers here together with their settings
 local servers = {
   bashls = {},
-  clangd = {},
-  cssls = {},
-  tsserver = {},
-  html = {},
-  jsonls = {},
+  -- clangd = {},
+  -- cssls = {},
+  -- tsserver = {},
+  -- html = {},
+  -- jsonls = {},
   pyright = {},
   yamlls = {},
   sumneko_lua = {
