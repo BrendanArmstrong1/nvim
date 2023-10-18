9313b2c8fde029ba4a2d78066437f25b3586cc4f
Brendan Armstrong <brendanarmstrong@proton.me>
2023-10-17 20:16:22 -0400

    added ALe and setup ale formatting and linting

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,252 +1,205 @@
 return {
-  -- lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    ---@class PluginLspOpts
-    opts = {
-      -- options for vim.diagnostic.config()
-      diagnostics = {
-        underline = true,
-        update_in_insert = false,
-        virtual_text = { spacing = 4, prefix = "●" },
-        severity_sort = true,
-      },
-      -- Automatically format on save
-      autoformat = true,
-      -- options for vim.lsp.buf.format
-      -- `bufnr` and `filter` is handled by the LazyVim formatter,
-      -- but can be also overridden when specified
-      format = {
-        formatting_options = nil,
-        timeout_ms = nil,
-      },
-      -- LSP Server Settings
-      servers = {
-        bashls = {},
-        -- clangd = {},
-        -- cssls = {},
-        -- tsserver = {},
-        -- html = {},
-        rust_analyzer = {
-          ["rust-analyzer"] = {
-            assist = {
-              importEnforceGranularity = true,
-              importPrefix = "crate",
-            },
-            cargo = {
-              features = "all",
-            },
-            checkOnSave = {
-              overrideCommand = { "cargo", "clippy", "--message-format=json" },
-            },
-            inlayHints = {
-              lifetimeElisionHints = {
-                enable = true,
-                useParameterNames = true,
-              },
-            },
-          },
-        },
-        pyright = {
-          root_dir = function(fname)
-            local root_files = {
-              ".git",
-              "pyproject.toml",
-              "setup.py",
-              "setup.cfg",
-              "requirements.txt",
-              "Pipfile",
-              "pyrightconfig.json",
-            }
-            local util = require("lspconfig/util")
-            return util.root_pattern(root_files.unpack)(fname)
-              or util.find_git_ancestor(fname)
-              or util.path.dirname(fname)
-          end,
-        },
-        -- yamlls = {},
-        -- selene = {},
-        lua_ls = {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-              },
-            },
-          },
-        },
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      setup = {
-        -- example to setup with typescript.nvim
-        -- tsserver = function(_, opts)
-        --   require("typescript").setup({ server = opts })
-        --   return true
-        -- end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    },
-    ---@param opts PluginLspOpts
-    config = function(_, opts)
-      -- setup autoformat
-      require("plugins.lsp.format").autoformat = opts.autoformat
-      -- setup formatting and keymaps
-      require("util").on_attach(function(client, buffer)
-        require("plugins.lsp.format").on_attach(client, buffer)
-        require("plugins.lsp.keymaps").on_attach(client, buffer)
-      end)
+	-- lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		---@class PluginLspOpts
+		opts = {
+			-- options for vim.diagnostic.config()
+			diagnostics = {
+				underline = true,
+				update_in_insert = false,
+				virtual_text = { spacing = 4, prefix = "●" },
+				severity_sort = true,
+			},
+			-- Automatically format on save
+			autoformat = true,
+			-- options for vim.lsp.buf.format
+			-- `bufnr` and `filter` is handled by the LazyVim formatter,
+			-- but can be also overridden when specified
+			format = {
+				formatting_options = nil,
+				timeout_ms = nil,
+			},
+			-- LSP Server Settings
+			servers = {
+				bashls = {},
+				-- clangd = {},
+				-- cssls = {},
+				-- tsserver = {},
+				-- html = {},
+				rust_analyzer = {
+					["rust-analyzer"] = {
+						assist = {
+							importEnforceGranularity = true,
+							importPrefix = "crate",
+						},
+						cargo = {
+							features = "all",
+						},
+						checkOnSave = {
+							overrideCommand = { "cargo", "clippy", "--message-format=json" },
+						},
+						inlayHints = {
+							lifetimeElisionHints = {
+								enable = true,
+								useParameterNames = true,
+							},
+						},
+					},
+				},
+				pylsp = {
+					settings = {
+						pylsp = {
+							plugins = {
+								pycodestyle = {
+									ignore = { "W391", "E742" },
+									maxLineLength = 80,
+								},
+							},
+						},
+					},
+				},
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+						},
+					},
+				},
+			},
+			-- you can do any additional lsp server setup here
+			-- return true if you don't want this server to be setup with lspconfig
+			setup = {
+				-- example to setup with typescript.nvim
+				-- tsserver = function(_, opts)
+				--   require("typescript").setup({ server = opts })
+				--   return true
+				-- end,
+				-- Specify * to use this function as a fallback for any server
+				-- ["*"] = function(server, opts) end,
+			},
+		},
+		---@param opts PluginLspOpts
+		config = function(_, opts)
+			-- setup formatting and keymaps
+			require("util").on_attach(function(client, buffer)
+				require("plugins.lsp.keymaps").on_attach(client, buffer)
+			end)
 
-      -- diagnostics
-      for name, icon in pairs(require("config.settings").icons.diagnostics) do
-        name = "DiagnosticSign" .. name
-        vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
-      end
-      vim.diagnostic.config(opts.diagnostics)
+			-- diagnostics
+			for name, icon in pairs(require("config.settings").icons.diagnostics) do
+				name = "DiagnosticSign" .. name
+				vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+			end
+			vim.diagnostic.config(opts.diagnostics)
 
-      local servers = opts.servers
-      local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local servers = opts.servers
+			local capabilities =
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-      local function setup(server)
-        local server_opts = vim.tbl_deep_extend("force", {
-          capabilities = vim.deepcopy(capabilities),
-        }, servers[server] or {})
+			local function setup(server)
+				local server_opts = vim.tbl_deep_extend("force", {
+					capabilities = vim.deepcopy(capabilities),
+				}, servers[server] or {})
 
-        if opts.setup[server] then
-          if opts.setup[server](server, server_opts) then
-            return
-          end
-        elseif opts.setup["*"] then
-          if opts.setup["*"](server, server_opts) then
-            return
-          end
-        end
-        require("lspconfig")[server].setup(server_opts)
-      end
+				if opts.setup[server] then
+					if opts.setup[server](server, server_opts) then
+						return
+					end
+				elseif opts.setup["*"] then
+					if opts.setup["*"](server, server_opts) then
+						return
+					end
+				end
+				require("lspconfig")[server].setup(server_opts)
+			end
 
-      local have_mason, mlsp = pcall(require, "mason-lspconfig")
-      local available = have_mason and mlsp.get_available_servers() or {}
+			local have_mason, mlsp = pcall(require, "mason-lspconfig")
+			local available = have_mason and mlsp.get_available_servers() or {}
 
-      local ensure_installed = {} ---@type string[]
-      for server, server_opts in pairs(servers) do
-        if server_opts then
-          server_opts = server_opts == true and {} or server_opts
-          -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-          if server_opts.mason == false or not vim.tbl_contains(available, server) then
-            setup(server)
-          else
-            ensure_installed[#ensure_installed + 1] = server
-          end
-        end
-      end
+			local ensure_installed = {} ---@type string[]
+			for server, server_opts in pairs(servers) do
+				if server_opts then
+					server_opts = server_opts == true and {} or server_opts
+					-- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+					if server_opts.mason == false or not vim.tbl_contains(available, server) then
+						setup(server)
+					else
+						ensure_installed[#ensure_installed + 1] = server
+					end
+				end
+			end
 
-      if have_mason then
-        mlsp.setup({ ensure_installed = ensure_installed })
-        mlsp.setup_handlers({ setup })
-      end
-    end,
-  },
+			if have_mason then
+				mlsp.setup({ ensure_installed = ensure_installed })
+				mlsp.setup_handlers({ setup })
+			end
+		end,
+	},
 
-  -- lsp symbol navigation for lualine
-  {
-    "SmiteshP/nvim-navic",
-    init = function()
-      vim.g.navic_silence = true
-      require("util").on_attach(function(client, buffer)
-        require("nvim-navic").attach(client, buffer)
-      end)
-    end,
-    opts = { separator = " ", highlight = true, depth_limit = 5 },
-  },
+	-- lsp symbol navigation for lualine
+	{
+		"SmiteshP/nvim-navic",
+		init = function()
+			vim.g.navic_silence = true
+			require("util").on_attach(function(client, buffer)
+				require("nvim-navic").attach(client, buffer)
+			end)
+		end,
+		opts = { separator = " ", highlight = true, depth_limit = 5 },
+	},
 
-  -- formatters
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = "BufReadPre",
-    dependencies = { "mason.nvim" },
-    config = function()
-      local null_ls = require("null-ls")
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-      local formatting = null_ls.builtins.formatting
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-      local diagnostics = null_ls.builtins.diagnostics
-      null_ls.setup({
-        debug = false,
-        debounce = 150,
-        save_after_format = false,
-        log_level = "info",
-        sources = {
-          -- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-          -- formatting.codespell.with({ filetype = { "markdown", "txt" } }),
+	-- -- formatters
 
-          -- python
-          formatting.black.with({ extra_args = { "--fast", "-l", 79 } }),
-          -- diagnostics.flake8.with({ extra_args = { "--extend-ignore=E203" } }),
-          diagnostics.ruff,
+	{
+		"dense-analysis/ale",
+		event = "BufReadPre",
+		config = function()
+			vim.g.ale_linters_explicit = 1
+			vim.g.ale_linters = {
+				python = { "ruff", "flake8", "mypy" },
+				rust = { "analyzer", "cargo" },
+			}
+			vim.g.ale_fix_on_save = 1
+			vim.g.ale_fixers = {
+				["*"] = { "remove_trailing_lines", "trim_whitespace" },
+				python = "black",
+				lua = { "stylua" },
+				rust = { "rustfmt" },
+			}
+		end,
+	},
 
-          -- shell
-          formatting.shfmt,
+	-- cmdline tools and lsp servers
+	{
 
-          -- c
-          formatting.clang_format,
-          formatting.cmake_format,
-
-          -- lua
-          -- diagnostics.selene,
-          formatting.stylua.with({ extra_args = { "--indent-type", "Spaces", "--indent-width", 2 } }),
-
-          -- rust
-          formatting.rustfmt.with({
-            extra_args = function(params)
-              local Path = require("plenary.path")
-              local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
-
-              if cargo_toml:exists() and cargo_toml:is_file() then
-                for _, line in ipairs(cargo_toml:readlines()) do
-                  local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
-                  if edition then
-                    return { "--edition=" .. edition }
-                  end
-                end
-              end
-              -- default edition when we don't find `Cargo.toml` or the `edition` in it.
-              return { "--edition=2021" }
-            end,
-          }),
-        },
-      })
-    end,
-  },
-
-  -- cmdline tools and lsp servers
-  {
-
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-    ensure_installed = {
-      "stylua",
-      -- "selene",
-      "black",
-      "flake8",
-      "rust-analyzer",
-    },
-    opts = function(plugin)
-      require("mason").setup()
-      local mr = require("mason-registry")
-      for _, tool in ipairs(plugin.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
-        end
-      end
-    end,
-  },
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+		ensure_installed = {
+			"stylua",
+			-- "selene",
+			"black",
+			"flake8",
+			"rust-analyzer",
+		},
+		opts = function(plugin)
+			require("mason").setup()
+			local mr = require("mason-registry")
+			for _, tool in ipairs(plugin.ensure_installed) do
+				local p = mr.get_package(tool)
+				if not p:is_installed() then
+					p:install()
+				end
+			end
+		end,
+	},
 }
--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -1,62 +1,61 @@
 local M = {}
 
 function M.on_attach(client, buffer)
-  local self = M.new(client, buffer)
+	local self = M.new(client, buffer)
 
-  self:map("<leader>vd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-  self:map("<leader>li", "LspInfo", { desc = "Lsp Info" })
-  self:map("gd", "Telescope lsp_definitions", { desc = "Goto Definition" })
-  self:map("gD", "Telescope lsp_declarations", { desc = "Goto Declaration" })
-  self:map("gI", "Telescope lsp_implementations", { desc = "Goto Implementation" })
-  self:map("gt", "Telescope lsp_type_definitions", { desc = "Goto Type Definition" })
-  self:map("K", vim.lsp.buf.hover, { desc = "Hover" })
-  self:map("]d", M.diagnostic_goto(true), { desc = "Next Diagnostic" })
-  self:map("[d", M.diagnostic_goto(false), { desc = "Prev Diagnostic" })
+	self:map("<leader>vd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+	self:map("<leader>li", "LspInfo", { desc = "Lsp Info" })
+	self:map("gd", "Telescope lsp_definitions", { desc = "Goto Definition" })
+	self:map("gD", "Telescope lsp_declarations", { desc = "Goto Declaration" })
 
-  self:map("<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Help", mode = { "i" }, has = "signatureHelp" })
-  self:map("<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", mode = { "n", "v" }, has = "codeAction" })
+	self:map("gI", "Telescope lsp_implementations", { desc = "Goto Implementation" })
+	self:map("gt", "Telescope lsp_type_definitions", { desc = "Goto Type Definition" })
+	self:map("K", vim.lsp.buf.hover, { desc = "Hover" })
+	self:map("]d", M.diagnostic_goto(true), { desc = "Next Diagnostic" })
+	self:map("[d", M.diagnostic_goto(false), { desc = "Prev Diagnostic" })
 
-  local format = require("plugins.lsp.format").format
-  self:map("<leader>cf", format, { desc = "Format Document", has = "documentFormatting" })
-  self:map("<leader>cf", format, { desc = "Format Range", mode = "v", has = "documentRangeFormatting" })
+	self:map("<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Help", mode = { "i" }, has = "signatureHelp" })
+	self:map("<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", mode = { "n", "v" }, has = "codeAction" })
+
+	self:map("<leader>cf", "ALEFix", { desc = "Format Document", has = "documentFormatting" })
 end
 
 function M.new(client, buffer)
-  return setmetatable({ client = client, buffer = buffer }, { __index = M })
+	return setmetatable({ client = client, buffer = buffer }, { __index = M })
 end
 
 function M:has(cap)
-  return self.client.server_capabilities[cap .. "Provider"]
+	return self.client.server_capabilities[cap .. "Provider"]
 end
 
 function M:map(lhs, rhs, opts)
-  opts = opts or {}
-  if opts.has and not self:has(opts.has) then
-    return
-  end
-  vim.keymap.set(
-    opts.mode or "n",
-    lhs,
-    type(rhs) == "string" and ("<cmd>%s<cr>"):format(rhs) or rhs,
-    { silent = true, buffer = self.buffer, expr = opts.expr, desc = opts.desc }
-  )
+	opts = opts or {}
+	if opts.has and not self:has(opts.has) then
+		return
+	end
+	vim.keymap.set(
+		opts.mode or "n",
+		lhs,
+		type(rhs) == "string" and ("<cmd>%s<cr>"):format(rhs) or rhs,
+		{ silent = true, buffer = self.buffer, expr = opts.expr, desc = opts.desc }
+	)
 end
 
 function M.rename()
-  if pcall(require, "inc_rename") then
-    return ":IncRename " .. vim.fn.expand("<cword>")
-  else
-    vim.lsp.buf.rename()
-  end
+	if pcall(require, "inc_rename") then
+		return ":IncRename " .. vim.fn.expand("<cword>")
+	else
+		vim.lsp.buf.rename()
+	end
 end
 
 function M.diagnostic_goto(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
+	local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+	severity = severity and vim.diagnostic.severity[severity] or nil
 
-  return function()
-    go({ severity = severity, float = false })
-  end
+	return function()
+		go({ severity = severity, float = false })
+	end
 end
 
 return M
