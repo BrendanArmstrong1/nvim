3860e249408e4d7ec976bbe51086b5dba3a7ff95
Brendan Armstrong <brendanarmstrong@proton.me>
2023-11-04 14:53:49 -0400

    working mode with visual <count> replace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,16 +5,16 @@ vim.keymap.set("n", "k", 'v:count1 > 4 ? "m\'" .. v:count .. "gkm\'" : "gk"', { 
 -- don't add '{' or '}' to jumplist.
 -- https://superuser.com/questions/836784/in-vim-dont-store-motions-in-jumplist
 vim.api.nvim_set_keymap(
-  "n",
-  "}",
-  "':<C-u> execute \"keepjumps norm! ' .. v:count1 .. '}\"<CR>'",
-  { expr = true, silent = true }
+	"n",
+	"}",
+	"':<C-u> execute \"keepjumps norm! ' .. v:count1 .. '}\"<CR>'",
+	{ expr = true, silent = true }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "{",
-  "':<C-u> execute \"keepjumps norm! ' .. v:count1 .. '{\"<CR>'",
-  { expr = true, silent = true }
+	"n",
+	"{",
+	"':<C-u> execute \"keepjumps norm! ' .. v:count1 .. '{\"<CR>'",
+	{ expr = true, silent = true }
 )
 
 -- Lazy access without triggering cmp lazy-load
@@ -28,58 +28,52 @@ vim.keymap.set("n", "<C-l>", "<nop>")
 -- nice replace macro, works with counts now
 -- "https://github.com/community/community/discussions/29817"
 vim.api.nvim_set_keymap(
-  "n",
-  "<leader>s",
-  "':' .. (v:count ? '' : '%') .. 's/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>'",
-  { desc = "replace word under cursor", noremap = true, expr = true }
+	"n",
+	"<leader>s",
+	"':' .. (v:count ? '' : '%') .. 's/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>'",
+	{ desc = "replace word under cursor", noremap = true, expr = true }
 )
 
--- [peters wedding]
--- [peters wedding]
--- [peters wedding]
--- [peters wedding]
--- [peters wedding]
--- [peters wedding]
-
 local function ReplaceVisualSelection()
-  local count = vim.v.count
-  local keys = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-  vim.api.nvim_feedkeys(keys, "x", false)
-  local function Set(list)
-    local set = {}
-    for _, l in ipairs(list) do
-      set[l] = true
-    end
-    return set
-  end
-  local escaped_keys = Set({ "[", "(", "." })
-  local vstart = vim.api.nvim_buf_get_mark(0, "<")
-  local vend = vim.api.nvim_buf_get_mark(0, ">")
-  local line = vim.api.nvim_buf_get_lines(0, vstart[1] - 1, vend[1], 0)
-  local text = string.sub(line[1], vstart[2] + 1, vend[2] + 1)
-  local escaped_string = text
-  local index = 0
-  for i = 1, string.len(text) do
-    local letter = string.sub(text, i, i)
-    if escaped_keys[letter] then
-      escaped_string = string.sub(escaped_string, 0, i - 1 + index)
-        .. "\\"
-        .. string.sub(escaped_string, i + index, string.len(escaped_string))
-      index = index + 1
-    end
-  end
+	local count = vim.v.count
+	local keys = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+	vim.api.nvim_feedkeys(keys, "x", false)
+	local function Set(list)
+		local set = {}
+		for _, l in ipairs(list) do
+			set[l] = true
+		end
+		return set
+	end
+	local escaped_keys = Set({"<", "[", "\\", "." })
+	local vstart = vim.api.nvim_buf_get_mark(0, "<")
+	local vend = vim.api.nvim_buf_get_mark(0, ">")
+	local line = vim.api.nvim_buf_get_lines(0, vstart[1] - 1, vend[1], 0)
+	local text = string.sub(line[1], vstart[2] + 1, vend[2] + 1)
+	local escaped_string = text
 
-  if count > 0 then
-    return ":<C-U>.,.+" .. count .. "s/" .. escaped_string .. "/" .. text .. "/gI<Left><Left><Left>"
+	local index = 0
+	for i = 1, string.len(text) do
+		local letter = string.sub(text, i, i)
+		if escaped_keys[letter] then
+			escaped_string = string.sub(escaped_string, 0, i - 1 + index)
+				.. "\\"
+				.. string.sub(escaped_string, i + index, string.len(escaped_string))
+			index = index + 1
+		end
+	end
+
+	if count > 0 then
+		return  ":<C-U>.,.+" .. count .. "s/" .. escaped_string .. "/" .. text .. "/gI<Left><Left><Left>"
   end
-  return ":<C-U>%s/" .. count .. escaped_string .. "/" .. text .. "/gI<Left><Left><Left>"
+  return  ":<C-U>%s/" .. escaped_string .. "/" .. text .. "/gI<Left><Left><Left>"
 end
 
 vim.keymap.set(
-  "x",
-  "<leader>s",
-  ReplaceVisualSelection,
-  { desc = "replace word under cursor", noremap = true, expr = true }
+	"x",
+	"<leader>s",
+	ReplaceVisualSelection,
+	{ desc = "replace visual selection", noremap = true, expr = true }
 )
 
 -- movement mappings
@@ -159,22 +153,22 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
 vim.keymap.set(
-  "n",
-  "<leader>cg",
-  '<CMD>call myfunc#ExecuteStuff("right")<CR>',
-  { desc = "execute bufffer(right)", noremap = true, silent = true }
+	"n",
+	"<leader>cg",
+	'<CMD>call myfunc#ExecuteStuff("right")<CR>',
+	{ desc = "execute bufffer(right)", noremap = true, silent = true }
 )
 vim.keymap.set(
-  "n",
-  "<leader>ch",
-  '<CMD>call myfunc#ExecuteStuff("bot")<CR>',
-  { desc = "execute buffer(bot)", noremap = true, silent = true }
+	"n",
+	"<leader>ch",
+	'<CMD>call myfunc#ExecuteStuff("bot")<CR>',
+	{ desc = "execute buffer(bot)", noremap = true, silent = true }
 )
 vim.keymap.set(
-  "n",
-  "<leader>cd",
-  "<CMD>call myfunc#CloseTerm()<CR>",
-  { desc = "close term", noremap = true, silent = true }
+	"n",
+	"<leader>cd",
+	"<CMD>call myfunc#CloseTerm()<CR>",
+	{ desc = "close term", noremap = true, silent = true }
 )
 
 -- new file
@@ -193,24 +187,24 @@ vim.keymap.set("n", "<leader>o", ":e <C-R>=expand('%:p:h') . '/' <CR>", { desc =
 local Util = require("lazy.core.util")
 local enabled = true
 function Toggle_diagnostics()
-  enabled = not enabled
-  if enabled then
-    vim.diagnostic.enable(0)
-  else
-    vim.diagnostic.disable(0)
-  end
+	enabled = not enabled
+	if enabled then
+		vim.diagnostic.enable(0)
+	else
+		vim.diagnostic.disable(0)
+	end
 end
 
-local fmt_enabled = true
+local fmt_enabled = false
 function Toggle_formatting()
-  fmt_enabled = not fmt_enabled
-  if fmt_enabled then
-    Util.info("Enabled format on save", { title = "Format" })
-    vim.b.autoformat = true
-  else
-    Util.info("Disabled format on save", { title = "Format" })
-    vim.b.autoformat = false
-  end
+	fmt_enabled = not fmt_enabled
+	if fmt_enabled then
+		Util.info("Enabled format on save", { title = "Format" })
+		vim.g.ale_fix_on_save = 1
+	else
+		Util.info("Disabled format on save", { title = "Format" })
+		vim.g.ale_fix_on_save = 0
+	end
 end
 
 -- toggle options
