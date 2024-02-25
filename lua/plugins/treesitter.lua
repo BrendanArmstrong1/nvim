501133d173fa764870a82589d4bc4bd8c80bd2cf
Brendan Armstrong <brendanarmstrong@proton.me>
2024-02-25 09:30:06 -0500

    fix: upgraded treesitter object move, upgraded advanced obects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,99 +1,148 @@
 return {
-  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = "BufReadPost",
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-context",
-        config = function()
-          require("treesitter-context").setup({
-            enable = true,
-          })
-        end,
-      },
-      {
-        "andymass/vim-matchup",
-        config = function()
-          vim.g.matchup_matchparen_offscreen = { method = "popup" }
-        end,
-      },
-    },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        auto_install = false,
-        sync_install = false,
-        ensure_installed = {
-          "bash",
-          "help",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "c",
-          "rust",
-          "latex",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "query",
-          "regex",
-          "tsx",
-          "typescript",
-          "vim",
-          "yaml",
-        },
-        highlight = {
-          enable = true,
-          disable = function(_, buf) -- "_" variable is lang
-            local max_filesize = 10000 * 1024 -- 1000 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              vim.notify("Tree sitter disabled")
-              return true
-            end
-          end,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-          disable = { "python" },
-        },
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "S",
-            node_incremental = "S",
-            scope_incremental = "gn",
-            node_decremental = "H",
-          },
-        },
-        query_linter = {
-          enable = true,
-          use_virtual_text = true,
-          lint_events = { "BufWrite", "CursorHold" },
-        },
-        endwise = {
-          enable = true,
-        },
-        textobjects = {
-          select = {
-            enable = false,
-          },
-          move = {
-            enable = false,
-          },
-          lsp_interop = {
-            enable = false,
-          },
-        },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		keys = {
+			{ "]m", mode = { "n", "x", "o" } },
+			{ "]M", mode = { "n", "x", "o" } },
+			{ "[M", mode = { "n", "x", "o" } },
+			{ "[M", mode = { "n", "x", "o" } },
+			{ "[[", mode = { "n", "x", "o" } },
+			{ "]]", mode = { "n", "x", "o" } },
+			{ "[]", mode = { "n", "x", "o" } },
+			{ "][", mode = { "n", "x", "o" } },
+		},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = { enable = false },
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = { query = "@class.outer", desc = "Next class start" },
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+				},
+			})
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = "BufReadPost",
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter-context",
+				config = function()
+					require("treesitter-context").setup({
+						enable = true,
+					})
+				end,
+			},
+			{
+				"andymass/vim-matchup",
+				config = function()
+					vim.g.matchup_matchparen_offscreen = { method = "popup" }
+				end,
+			},
+		},
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = false,
+				sync_install = false,
+				ensure_installed = {
+					"bash",
+					"help",
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"c",
+					"rust",
+					"latex",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"query",
+					"regex",
+					"tsx",
+					"typescript",
+					"vim",
+					"yaml",
+				},
+				highlight = {
+					enable = true,
+					disable = function(_, buf) -- "_" variable is lang
+						local max_filesize = 10000 * 1024 -- 1000 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							vim.notify("Tree sitter disabled")
+							return true
+						end
+					end,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = true,
+					disable = { "python" },
+				},
+				context_commentstring = {
+					enable = true,
+					enable_autocmd = false,
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "S",
+						node_incremental = "S",
+						scope_incremental = "gn",
+						node_decremental = "H",
+					},
+				},
+				query_linter = {
+					enable = true,
+					use_virtual_text = true,
+					lint_events = { "BufWrite", "CursorHold" },
+				},
+				endwise = {
+					enable = true,
+				},
+				textobjects = {
+					select = {
+						enable = false,
+					},
+					move = {
+						enable = false,
+					},
+					lsp_interop = {
+						enable = false,
+					},
+				},
+			})
+		end,
+	},
 }
--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,34 +1,35 @@
 return {
-  -- surround
-  {
-    "echasnovski/mini.surround",
-    keys = {
-      "gz",
-      "dz",
-      "cz",
-      "vz",
-      {
-        "Z",
-        ":<C-u>lua MiniSurround.add('visual')<CR>",
-        mode = "x",
-        silent = true,
-      },
-    },
-    init = function()
-      -- Remap adding surrounding to Visual mode selection
-      -- vim.api.nvim_set_keymap("x", "Z", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
-      -- Make special mapping for "add surrounding for line"
-      vim.api.nvim_set_keymap("n", "gzz", "gz_", { noremap = false })
-      vim.api.nvim_set_keymap(
-        "i",
-        "<C-_>",
-        "<C-o>h<C-o>gziw_<C-o>gziw_<C-o>E<C-o>a",
-        { desc = "dunder(python)", noremap = false }
-      )
-    end,
-    config = function()
-      -- use gz mappings instead of s to prevent conflict with leap
-      require("mini.surround").setup({
+	-- surround
+	{
+		"echasnovski/mini.surround",
+		keys = {
+			"gz",
+			"dz",
+			"cz",
+			"vz",
+			{
+				"Z",
+				":<C-u>lua MiniSurround.add('visual')<CR>",
+				mode = "x",
+				silent = true,
+			},
+		},
+		init = function()
+			-- Remap adding surrounding to Visual mode selection
+			-- vim.api.nvim_set_keymap("x", "Z", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
+			-- Make special mapping for "add surrounding for line"
+      --
+			vim.api.nvim_set_keymap("n", "gzz", "gz_", { noremap = false })
+			vim.api.nvim_set_keymap(
+				"i",
+				"<C-_>",
+				"<C-o>h<C-o>gziw_<C-o>gziw_<C-o>E<C-o>a",
+				{ desc = "dunder(python)", noremap = false }
+			)
+		end,
+		config = function()
+			-- use gz mappings instead of s to prevent conflict with leap
+			require("mini.surround").setup({
           --stylua: ignore
           mappings = {
             add            = "gz", -- Add surrounding in Normal and Visual modes
@@ -39,55 +40,67 @@ return {
             replace        = "cz", -- Replace surrounding
             update_n_lines = "", -- Update `n_lines`
           },
-      })
-    end,
-  },
+			})
+		end,
+	},
 
-  -- todo comments
-  {
-    "folke/todo-comments.nvim",
-    cmd = { "TodoTrouble", "TodoTelescope" },
-    event = { "BufReadPost", "BufNewFile" },
-    config = true,
+	-- todo comments
+	{
+		"folke/todo-comments.nvim",
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		event = { "BufReadPost", "BufNewFile" },
+		config = true,
     -- stylua: ignore
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
       { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
       { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>St", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-      { "<leader>ST", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
     },
-  },
+	},
 
-  -- comments
-  { "JoosepAlviste/nvim-ts-context-commentstring" },
-  {
-    "echasnovski/mini.comment",
-    keys = { { "gc", mode = { "n", "x" } } },
-    config = function()
-      require("mini.comment").setup({
-        hooks = {
-          pre = function()
-            require("ts_context_commentstring.internal").update_commentstring({})
-          end,
-        },
-      })
-    end,
-  },
+	-- comments
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.comment",
+		keys = { { "gc", mode = { "n", "x" } } },
+		config = function()
+			require("mini.comment").setup({
+				options = {
+					custom_commentstring = function()
+						return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+					end,
+					ignore_blank_line = true,
+				},
+				mappings = {
+					comment = "gc",
+					commentine = "gc",
+					comment_visual = "gc",
+					textobject = "gc",
+				},
+			})
+		end,
+	},
 
-  -- auto pairs
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
+	-- auto pairs
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
         --stylua: ignore
         require("nvim-autopairs").setup({
           fast_wrap = {
             map            = "<c-s>",
             chars          = { "{", "[", "(", '"', "'", "`" },
             pattern        = [=[[%'%"%)%>%]%)%}%,]]=],
-            end_key        = "$",
+            end_key        = "L",
             keys           = "aoeuidhtnsfgcrl",
             check_comma    = true,
             highlight      = "Search",
@@ -110,145 +123,146 @@ return {
           map_c_h                   = false, -- Map the <C-h> key to delete a pair
           map_c_w                   = false, -- map <c-w> to delete a pair iff possible
         })
-      local endwise = require("nvim-autopairs.ts-rule").endwise
-      local npairs = require("nvim-autopairs")
-      npairs.add_rules({
-        endwise("then$", "end", "lua", "if_statement"),
-      })
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp_status_ok, cmp = pcall(require, "cmp")
-      if not cmp_status_ok then
-        return
-      end
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
-    end,
-  },
+			local endwise = require("nvim-autopairs.ts-rule").endwise
+			local npairs = require("nvim-autopairs")
+			npairs.add_rules({
+				endwise("then$", "end", "lua", "if_statement"),
+			})
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp_status_ok, cmp = pcall(require, "cmp")
+			if not cmp_status_ok then
+				return
+			end
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+		end,
+	},
 
-  -- Split Join
-  {
-    "Wansmer/treesj",
-    cmd = {
-      "TSJToggle",
-      "TSJSplit",
-      "TSJJoin",
-    },
-    keys = {
-      { "gss", "<cmd>TSJSplit<cr>" },
-      { "gsj", "<cmd>TSJJoin<cr>" },
-      { "gj", "<cmd>TSJToggle<cr>" },
-    },
-    config = function()
-      require("treesj").setup({
-        -- Use default keymaps
-        -- (<space>m - toggle, <space>j - join, <space>s - split)
-        use_default_keymaps = false,
+	-- Split Join
+	{
+		"Wansmer/treesj",
+		cmd = {
+			"TSJToggle",
+			"TSJSplit",
+			"TSJJoin",
+		},
+    -- stylua: ignore
+		keys = {
+			{ "gss", function() require('treesj').split() end },
+			{ "gsj", function() require('treesj').join() end },
+      { "gm", function() require('treesj').toggle({ split = { recursive = true } }) end },
+		},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesj").setup({
+				-- Use default keymaps
+				-- (<space>m - toggle, <space>j - join, <space>s - split)
+				use_default_keymaps = false,
 
-        -- Node with syntax error will not be formatted
-        check_syntax_error = true,
+				-- Node with syntax error will not be formatted
+				check_syntax_error = true,
 
-        -- If line after join will be longer than max value,
-        -- node will not be formatted
-        max_join_length = 120,
+				-- If line after join will be longer than max value,
+				-- node will not be formatted
+				max_join_length = 120,
 
-        -- hold|start|end:
-        -- hold - cursor follows the node/place on which it was called
-        -- start - cursor jumps to the first symbol of the node being formatted
-        -- end - cursor jumps to the last symbol of the node being formatted
-        cursor_behavior = "hold",
+				-- hold|start|end:
+				-- hold - cursor follows the node/place on which it was called
+				-- start - cursor jumps to the first symbol of the node being formatted
+				-- end - cursor jumps to the last symbol of the node being formatted
+				cursor_behavior = "hold",
 
-        -- Notify about possible problems or not
-        notify = true,
-        -- langs = langs,
-      })
-    end,
-  },
+				-- Notify about possible problems or not
+				notify = true,
+				-- langs = langs,
+				dot_repeat = true,
+			})
+		end,
+	},
 
-  -- better text-objects
-  {
-    "echasnovski/mini.ai",
-    keys = {
-      { "a", mode = { "x", "o" } },
-      { "i", mode = { "x", "o" } },
-    },
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        init = function()
-          -- no need to load the plugin, since we only need its queries
-          require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-        end,
-      },
-    },
-    opts = function()
-      local ai = require("mini.ai")
-      return {
-        n_lines = 500,
-        custom_textobjects = {
-            -- stylua: ignore
-            o = ai.gen_spec.treesitter({
-              a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-              i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-            }, {}),
-          f = ai.gen_spec.treesitter({
-            a = "@function.outer",
-            i = "@function.inner",
-          }, {}),
-          c = ai.gen_spec.treesitter({
-            a = "@class.outer",
-            i = "@class.inner",
-          }, {}),
-        },
-      }
-    end,
-    config = function(_, opts)
-      local ai = require("mini.ai")
-      ai.setup(opts)
-    end,
-  },
+	-- better text-objects
+	{
+		"echasnovski/mini.ai",
+		keys = {
+			{ "a", mode = { "x", "o" } },
+			{ "i", mode = { "x", "o" } },
+		},
+		dependencies = { { "nvim-treesitter/nvim-treesitter-textobjects" } },
+		opts = function()
+			local ai = require("mini.ai")
+			return {
+				n_lines = 500,
+				custom_textobjects = {
+					b = ai.gen_spec.treesitter({
+						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+					}, {}),
+					f = ai.gen_spec.treesitter({
+						a = "@function.outer",
+						i = "@function.inner",
+					}, {}),
+					c = ai.gen_spec.treesitter({
+						a = "@class.outer",
+						i = "@class.inner",
+					}, {}),
+				},
+				mappings = {
+					around = "a",
+					inside = "i",
+					goto_left = "g[",
+					goto_right = "g]",
+				},
+				search_method = "cover_or_nearest",
+			}
+		end,
+		config = function(_, opts)
+			local ai = require("mini.ai")
+			ai.setup(opts)
+		end,
+	},
 
-  -- session management
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+	-- session management
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
     -- stylua: ignore
     keys = {
       { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
       { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
       { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
-  },
+	},
 
-  -- easy align
-  {
-    "junegunn/vim-easy-align",
-    keys = {
-      { "gl", "<Plug>(EasyAlign)", mode = { "n", "x" } },
-    },
-  },
+	-- easy align
+	{
+		"junegunn/vim-easy-align",
+		keys = {
+			{ "gl", "<Plug>(EasyAlign)", mode = { "n", "x" } },
+		},
+	},
 
-  -- vim-rsi
-  {
-    "tpope/vim-rsi",
-    event = {
-      "InsertEnter",
-      "CmdlineEnter",
-    },
-  },
+	-- vim-rsi
+	{
+		"tpope/vim-rsi",
+		event = {
+			"InsertEnter",
+			"CmdlineEnter",
+		},
+	},
 
-  {
-    "tpope/vim-unimpaired",
-    event = {
-      "InsertEnter",
-      "CmdlineEnter",
-    },
-    keys = {
-      { "[<Space>" },
-      { "]<Space>" },
-    },
-  },
-  {
-    "tpope/vim-repeat",
-    event = "VeryLazy",
-  },
+	{
+		"tpope/vim-unimpaired",
+		event = {
+			"InsertEnter",
+			"CmdlineEnter",
+		},
+		keys = {
+			{ "[<Space>" },
+			{ "]<Space>" },
+		},
+	},
+	{
+		"tpope/vim-repeat",
+		event = "VeryLazy",
+	},
 }
