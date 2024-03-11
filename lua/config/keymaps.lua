f451181ef9f8b5f456157fd47a81d54615e1becc
Brendan Armstrong <brendanarmstrong@proton.me>
2024-03-11 17:50:31 -0400

    clean: remove code that was transformed into plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,47 +33,48 @@ vim.api.nvim_set_keymap(
 	"':' .. (v:count ? '' : '%') .. 's/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>'",
 	{ desc = "replace word under cursor", noremap = true, expr = true }
 )
-local function ReplaceVisualSelection()
-	local count = vim.v.count
-	local keys = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-	vim.api.nvim_feedkeys(keys, "x", false)
-	local function Set(list)
-		local set = {}
-		for _, l in ipairs(list) do
-			set[l] = true
-		end
-		return set
-	end
-	local escaped_keys = Set({ "<", "[", "\\", "." })
-	local vstart = vim.api.nvim_buf_get_mark(0, "<")
-	local vend = vim.api.nvim_buf_get_mark(0, ">")
-	local line = vim.api.nvim_buf_get_lines(0, vstart[1] - 1, vend[1], 0)
-	local text = string.sub(line[1], vstart[2] + 1, vend[2] + 1)
-	local escaped_string = text
 
-	local index = 0
-	for i = 1, string.len(text) do
-		local letter = string.sub(text, i, i)
-		if escaped_keys[letter] then
-			escaped_string = string.sub(escaped_string, 0, i - 1 + index)
-				.. "\\"
-				.. string.sub(escaped_string, i + index, string.len(escaped_string))
-			index = index + 1
-		end
-	end
-
-	if count > 0 then
-		return ":<C-U>.,.+" .. count .. "s/" .. escaped_string .. "/" .. text .. "/gI<Left><Left><Left>"
-	end
-	return ":<C-U>%s/" .. escaped_string .. "/" .. text .. "/gI<Left><Left><Left>"
-end
-
-vim.keymap.set(
-	"x",
-	"<leader>s",
-	ReplaceVisualSelection,
-	{ desc = "replace visual selection", noremap = true, expr = true }
-)
+-- local function ReplaceVisualSelection()
+-- 	local count = vim.v.count
+-- 	local keys = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+-- 	vim.api.nvim_feedkeys(keys, "x", false)
+-- 	local function Set(list)
+-- 		local set = {}
+-- 		for _, l in ipairs(list) do
+-- 			set[l] = true
+-- 		end
+-- 		return set
+-- 	end
+-- 	local escaped_keys = Set({ "<", "[", "\\", "." })
+-- 	local vstart = vim.api.nvim_buf_get_mark(0, "<")
+-- 	local vend = vim.api.nvim_buf_get_mark(0, ">")
+-- 	local line = vim.api.nvim_buf_get_lines(0, vstart[1] - 1, vend[1], 0)
+-- 	local text = string.sub(line[1], vstart[2] + 1, vend[2] + 1)
+-- 	local escaped_string = text
+--
+-- 	local index = 0
+-- 	for i = 1, string.len(text) do
+-- 		local letter = string.sub(text, i, i)
+-- 		if escaped_keys[letter] then
+-- 			escaped_string = string.sub(escaped_string, 0, i - 1 + index)
+-- 				.. "\\"
+-- 				.. string.sub(escaped_string, i + index, string.len(escaped_string))
+-- 			index = index + 1
+-- 		end
+-- 	end
+--
+-- 	if count > 0 then
+-- 		return ":<C-U>.,.+" .. count .. "s/" .. escaped_string .. "/" .. text .. "/gI<Left><Left><Left>"
+-- 	end
+-- 	return ":<C-U>%s/" .. escaped_string .. "/" .. text .. "/gI<Left><Left><Left>"
+-- end
+--
+-- vim.keymap.set(
+-- 	"x",
+-- 	"<leader>s",
+-- 	ReplaceVisualSelection,
+-- 	{ desc = "replace visual selection", noremap = true, expr = true }
+-- )
 
 -- movement mappings
 vim.keymap.set({ "n", "x" }, "<C-e>", "repeat('<C-e>', 5)", { noremap = true, expr = true })
@@ -170,7 +171,12 @@ function Print_tmux_panes()
 	end
 end
 
+function Test_function()
+  local str = vim.api.nvim_replace_termcodes(":hello worl<Left><Left>",true,false,true)
+  vim.api.nvim_feedkeys(str, "t", false)
+end
 vim.keymap.set("n", "gy", Print_tmux_panes)
+vim.keymap.set("n", "g.", Test_function)
 
 local Util = require("lazy.core.util")
 local enabled = true
