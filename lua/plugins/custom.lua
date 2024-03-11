f56f7fb3a43ef87c382726e9da5b1f9c24a05eab
Brendan Armstrong <brendanarmstrong@proton.me>
2024-03-11 17:49:51 -0400

    feat: add new plugin to the repo

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -5,4 +5,17 @@ return {
 		dev = false,
 		config = true,
 	},
+	{
+		"BrendanArmstrong1/visual_replace.nvim",
+		keys = {
+			{
+				"<leader>s",
+				function()
+					require("visual_replace").replace_visual()
+				end,
+				mode = "x",
+			},
+		},
+		dev = false,
+	},
 }
