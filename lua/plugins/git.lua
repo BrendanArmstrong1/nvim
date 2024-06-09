e7086939137eea0998e058446d208639b7aff5dd
Brendan Armstrong <brendanarmstrong@proton.me>
2024-06-09 16:08:07 -0400

    added vim-rhubarb and updated entire git plugin section apparently

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,124 +1,127 @@
 return {
-  -- Fugitive
-  {
-    "tpope/vim-fugitive",
-    cmd = {"G", "Gedit", "Gvsplit"},
-    init = function()
-      vim.api.nvim_set_keymap("n", "<leader>gD", "<CMD>Gdiffsplit!<CR>", {})
-      vim.api.nvim_set_keymap("n", "<leader>ga", "<CMD>diffget //2<CR>", {})
-      vim.api.nvim_set_keymap("n", "<leader>gi", "<CMD>diffget //3<CR>", {})
-    end,
-    keys = {
-      { "<leader>gp", "<CMD>Git push<CR>" },
-      { "<leader>gc", "<CMD>Git commit<CR>" },
-      { "<leader>gg", "<CMD>Git<CR>" },
-      { "<leader>gL", "<CMD>Git log<CR>" },
-      { "<leader>gb", "<CMD>Git blame<CR>" },
-    },
-  },
+	-- Fugitive
+	{
+		"tpope/vim-fugitive",
+		dependencies = {
+			"tpope/vim-rhubarb",
+		},
+		cmd = { "G", "Gedit", "Gvsplit", "GBrowse" },
+		init = function()
+			vim.api.nvim_set_keymap("n", "<leader>gD", "<CMD>Gdiffsplit!<CR>", {})
+			vim.api.nvim_set_keymap("n", "<leader>ga", "<CMD>diffget //2<CR>", {})
+			vim.api.nvim_set_keymap("n", "<leader>gi", "<CMD>diffget //3<CR>", {})
+		end,
+		keys = {
+			{ "<leader>gp", "<CMD>Git push<CR>" },
+			{ "<leader>gc", "<CMD>Git commit<CR>" },
+			{ "<leader>gg", "<CMD>Git<CR>" },
+			{ "<leader>gL", "<CMD>Git log<CR>" },
+			{ "<leader>gb", "<CMD>Git blame<CR>" },
+		},
+	},
 
-  -- Diffview
-  {
-    "sindrets/diffview.nvim",
-    cmd = "DiffviewOpen",
-    keys = {
-      { "<leader>gd", "<cmd>DiffviewFileHistory %<cr>", desc = "open diff of file" },
-      { "<leader>GG", "<cmd>DiffviewOpen<cr>", desc = "open full diff" },
-    },
-    config = function()
-      -- Lua
-      local actions = require("diffview.actions")
+	-- Diffview
+	{
+		"sindrets/diffview.nvim",
+		cmd = "DiffviewOpen",
+		keys = {
+			{ "<leader>gd", "<cmd>DiffviewFileHistory %<cr>", desc = "open diff of file" },
+			{ "<leader>GG", "<cmd>DiffviewOpen<cr>", desc = "open full diff" },
+		},
+		config = function()
+			-- Lua
+			local actions = require("diffview.actions")
 
-      require("diffview").setup({
-        diff_binaries = false, -- Show diffs for binaries
-        enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
-        git_cmd = { "git" }, -- The git executable followed by default args.
-        hg_cmd = { "hg" }, -- The hg executable followed by default args.
-        use_icons = true, -- Requires nvim-web-devicons
-        show_help_hints = true, -- Show hints for how to open the help panel
-        watch_index = true, -- Update views and index buffers when the git index changes.
-        icons = { -- Only applies when use_icons is true.
-          folder_closed = "",
-          folder_open = "",
-        },
-        signs = {
-          fold_closed = "",
-          fold_open = "",
-          done = "✓",
-        },
-        view = {
-          -- Configure the layout and behavior of different types of views.
-          -- Available layouts:
-          --  'diff1_plain'
-          --    |'diff2_horizontal'
-          --    |'diff2_vertical'
-          --    |'diff3_horizontal'
-          --    |'diff3_vertical'
-          --    |'diff3_mixed'
-          --    |'diff4_mixed'
-          -- For more info, see ':h diffview-config-view.x.layout'.
-          default = {
-            -- Config for changed files, and staged files in diff views.
-            layout = "diff2_horizontal",
-            winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
-          },
-          merge_tool = {
-            -- Config for conflicted files in diff views during a merge or rebase.
-            layout = "diff3_horizontal",
-            disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
-            winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
-          },
-          file_history = {
-            -- Config for changed files in file history views.
-            layout = "diff2_horizontal",
-            winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
-          },
-        },
-        file_panel = {
-          listing_style = "tree", -- One of 'list' or 'tree'
-          tree_options = { -- Only applies when listing_style is 'tree'
-            flatten_dirs = true, -- Flatten dirs that only contain one single dir
-            folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
-          },
-          win_config = { -- See ':h diffview-config-win_config'
-            position = "left",
-            width = 35,
-            win_opts = {},
-          },
-        },
-        file_history_panel = {
-          log_options = { -- See ':h diffview-config-log_options'
-            git = {
-              single_file = {
-                diff_merges = "combined",
-              },
-              multi_file = {
-                diff_merges = "first-parent",
-              },
-            },
-            hg = {
-              single_file = {},
-              multi_file = {},
-            },
-          },
-          win_config = { -- See ':h diffview-config-win_config'
-            position = "bottom",
-            height = 16,
-            win_opts = {},
-          },
-        },
-        commit_log_panel = {
-          win_config = { -- See ':h diffview-config-win_config'
-            win_opts = {},
-          },
-        },
-        default_args = { -- Default args prepended to the arg-list for the listed commands
-          DiffviewOpen = {},
-          DiffviewFileHistory = {},
-        },
-        hooks = {}, -- See ':h diffview-config-hooks'
-        keymaps = {
-          disable_defaults = false, -- Disable the default keymaps
+			require("diffview").setup({
+				diff_binaries = false, -- Show diffs for binaries
+				enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
+				git_cmd = { "git" }, -- The git executable followed by default args.
+				hg_cmd = { "hg" }, -- The hg executable followed by default args.
+				use_icons = true, -- Requires nvim-web-devicons
+				show_help_hints = true, -- Show hints for how to open the help panel
+				watch_index = true, -- Update views and index buffers when the git index changes.
+				icons = { -- Only applies when use_icons is true.
+					folder_closed = "",
+					folder_open = "",
+				},
+				signs = {
+					fold_closed = "",
+					fold_open = "",
+					done = "✓",
+				},
+				view = {
+					-- Configure the layout and behavior of different types of views.
+					-- Available layouts:
+					--  'diff1_plain'
+					--    |'diff2_horizontal'
+					--    |'diff2_vertical'
+					--    |'diff3_horizontal'
+					--    |'diff3_vertical'
+					--    |'diff3_mixed'
+					--    |'diff4_mixed'
+					-- For more info, see ':h diffview-config-view.x.layout'.
+					default = {
+						-- Config for changed files, and staged files in diff views.
+						layout = "diff2_horizontal",
+						winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
+					},
+					merge_tool = {
+						-- Config for conflicted files in diff views during a merge or rebase.
+						layout = "diff3_horizontal",
+						disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
+						winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
+					},
+					file_history = {
+						-- Config for changed files in file history views.
+						layout = "diff2_horizontal",
+						winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
+					},
+				},
+				file_panel = {
+					listing_style = "tree", -- One of 'list' or 'tree'
+					tree_options = { -- Only applies when listing_style is 'tree'
+						flatten_dirs = true, -- Flatten dirs that only contain one single dir
+						folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
+					},
+					win_config = { -- See ':h diffview-config-win_config'
+						position = "left",
+						width = 35,
+						win_opts = {},
+					},
+				},
+				file_history_panel = {
+					log_options = { -- See ':h diffview-config-log_options'
+						git = {
+							single_file = {
+								diff_merges = "combined",
+							},
+							multi_file = {
+								diff_merges = "first-parent",
+							},
+						},
+						hg = {
+							single_file = {},
+							multi_file = {},
+						},
+					},
+					win_config = { -- See ':h diffview-config-win_config'
+						position = "bottom",
+						height = 16,
+						win_opts = {},
+					},
+				},
+				commit_log_panel = {
+					win_config = { -- See ':h diffview-config-win_config'
+						win_opts = {},
+					},
+				},
+				default_args = { -- Default args prepended to the arg-list for the listed commands
+					DiffviewOpen = {},
+					DiffviewFileHistory = {},
+				},
+				hooks = {}, -- See ':h diffview-config-hooks'
+				keymaps = {
+					disable_defaults = false, -- Disable the default keymaps
           --stylua: ignore
           view = {
             -- The `view` bindings are active in the diff buffers, only when the current
@@ -253,88 +256,88 @@ return {
             { "n", "q", actions.close, { desc = "Close help menu" } },
             { "n", "<esc>", actions.close, { desc = "Close help menu" } },
           },
-        },
-      })
-    end,
-  },
+				},
+			})
+		end,
+	},
 
-  -- git signs
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "BufReadPre",
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add = { text = "▎" },
-          change = { text = "▎" },
-          changedelete = { text = "▎" },
-          untracked = { text = "▎" },
-        },
-        signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-        numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-        watch_gitdir = {
-          interval = 1000,
-          follow_files = true,
-        },
-        attach_to_untracked = true,
-        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-          delay = 1000,
-          ignore_whitespace = false,
-        },
-        current_line_blame_formatter_opts = {
-          relative_time = false,
-        },
-        sign_priority = 6,
-        update_debounce = 100,
-        status_formatter = nil, -- Use default
-        max_file_length = 40000,
-        preview_config = {
-          -- Options passed to nvim_open_win
-          border = "single",
-          style = "minimal",
-          relative = "cursor",
-          row = 0,
-          col = 1,
-        },
-        yadm = {
-          enable = false,
-        },
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
+	-- git signs
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufReadPre",
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "▎" },
+					change = { text = "▎" },
+					changedelete = { text = "▎" },
+					untracked = { text = "▎" },
+				},
+				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+				numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+				word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+				watch_gitdir = {
+					interval = 1000,
+					follow_files = true,
+				},
+				attach_to_untracked = true,
+				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+				},
+				current_line_blame_formatter_opts = {
+					relative_time = false,
+				},
+				sign_priority = 6,
+				update_debounce = 100,
+				status_formatter = nil, -- Use default
+				max_file_length = 40000,
+				preview_config = {
+					-- Options passed to nvim_open_win
+					border = "single",
+					style = "minimal",
+					relative = "cursor",
+					row = 0,
+					col = 1,
+				},
+				yadm = {
+					enable = false,
+				},
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
 
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
 
-          -- Navigation
-          map("n", "]h", function()
-            if vim.wo.diff then
-              return "]h"
-            end
-            vim.schedule(function()
-              gs.next_hunk()
-              vim.cmd("norm! zz")
-            end)
-            return "<Ignore>"
-          end, { expr = true })
+					-- Navigation
+					map("n", "]h", function()
+						if vim.wo.diff then
+							return "]h"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+							vim.cmd("norm! zz")
+						end)
+						return "<Ignore>"
+					end, { expr = true })
 
-          map("n", "[h", function()
-            if vim.wo.diff then
-              return "[h"
-            end
-            vim.schedule(function()
-              gs.prev_hunk()
-              vim.cmd("norm! zz")
-            end)
-            return "<Ignore>"
-          end, { expr = true })
+					map("n", "[h", function()
+						if vim.wo.diff then
+							return "[h"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+							vim.cmd("norm! zz")
+						end)
+						return "<Ignore>"
+					end, { expr = true })
 
           -- Actions
           -- stylua: ignore start
@@ -352,9 +355,9 @@ return {
           map("n", "<leader>ht", gs.toggle_deleted, {silent=true})
           -- Text object
           map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-          -- stylua: ignore end
-        end,
-      })
-    end,
-  },
+					-- stylua: ignore end
+				end,
+			})
+		end,
+	},
 }
