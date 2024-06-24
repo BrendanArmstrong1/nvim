c847524049d1eeaef860d662707ce201a194e458
Brendan Armstrong <brendanarmstrong@proton.me>
2024-06-24 17:19:22 -0400

    add converter with "BCtohex" cmd and removed pinned treesitter version

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -17,4 +17,9 @@ M3.keys = {
 	},
 }
 
-return { M1, M2, M3 }
+local M4 = { "BrendanArmstrong1/base_converter.nvim"}
+M4.dev = true
+M4.cmd = {"BCtohex"}
+M4.config = true
+
+return { M1, M2, M3, M4 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -49,7 +49,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		event = "BufReadPost",
-    commit = "aa0c7dd29631ee8c2df738d99bd33b762b3a6e22",
+    -- commit = "aa0c7dd29631ee8c2df738d99bd33b762b3a6e22",
 		dependencies = {
 			{
 				"nvim-treesitter/nvim-treesitter-context",
