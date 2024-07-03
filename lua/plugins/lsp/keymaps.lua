70a66ce3ca9bb3533e218e725bfa3e258eaba568
Brendan Armstrong <brendanarmstrong@proton.me>
2024-07-03 18:57:30 -0400

    change M to local to prevent Variable clobering? I don't really know why this works but it does

--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -1,4 +1,33 @@
-M = {}
+local M = {}
+
+function M.new(client, buffer)
+  return setmetatable({ client = client, buffer = buffer }, { __index = M })
+end
+
+function M:has(cap)
+  return self.client.server_capabilities[cap .. "Provider"]
+end
+
+function M:map(lhs, rhs, opts)
+  opts = opts or {}
+  if opts.has and not self:has(opts.has) then
+    return
+  end
+  vim.keymap.set(
+    opts.mode or "n",
+    lhs,
+    type(rhs) == "string" and ("<cmd>%s<cr>"):format(rhs) or rhs,
+    { silent = true, buffer = self.buffer, expr = opts.expr, desc = opts.desc }
+  )
+end
+
+function M.rename()
+  if pcall(require, "inc_rename") then
+    return ":IncRename " .. vim.fn.expand("<cword>")
+  else
+    vim.lsp.buf.rename()
+  end
+end
 
 function M.on_attach(client, buffer)
 	local self = M.new(client, buffer)
@@ -98,34 +127,6 @@ function M.on_attach(client, buffer)
   -- end
 end
 
-function M.new(client, buffer)
-	return setmetatable({ client = client, buffer = buffer }, { __index = M })
-end
-
-function M:has(cap)
-	return self.client.server_capabilities[cap .. "Provider"]
-end
-
-function M:map(lhs, rhs, opts)
-	opts = opts or {}
-	if opts.has and not self:has(opts.has) then
-		return
-	end
-	vim.keymap.set(
-		opts.mode or "n",
-		lhs,
-		type(rhs) == "string" and ("<cmd>%s<cr>"):format(rhs) or rhs,
-		{ silent = true, buffer = self.buffer, expr = opts.expr, desc = opts.desc }
-	)
-end
-
-function M.rename()
-	if pcall(require, "inc_rename") then
-		return ":IncRename " .. vim.fn.expand("<cword>")
-	else
-		vim.lsp.buf.rename()
-	end
-end
 
 
 return M
