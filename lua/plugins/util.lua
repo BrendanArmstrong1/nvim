6c4f30d713bf7962ca9587374e4551ad384aae1c
Brendan Armstrong <brendanarmstrong@proton.me>
2024-09-17 18:10:59 -0400

    change block select to "o" key so I have the "q" and "b" keys back

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -147,7 +147,7 @@ return {
 			return {
 				n_lines = 500,
 				custom_textobjects = {
-					b = ai.gen_spec.treesitter({
+					o = ai.gen_spec.treesitter({
 						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
 						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
 					}, {}),
