ed3848a4332a7683f0b324149d0354314d8d6f4a
Brendan Armstrong <brendanarmstrong@proton.me>
2024-11-10 16:32:42 -0500

    change the method jumping hotkeys back to normal

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -24,19 +24,19 @@ return {
 						enable = true,
 						set_jumps = true, -- whether to set jumps in the jumplist
 						goto_next_start = {
-							["]m"] = "@function.outer",
+							["]-"] = "@function.outer",
 							["]]"] = { query = "@class.outer", desc = "Next class start" },
 						},
 						goto_next_end = {
-							["]M"] = "@function.outer",
+              ["]+"] = "@function.outer",
 							["]["] = "@class.outer",
 						},
 						goto_previous_start = {
-							["[m"] = "@function.outer",
+							["[-"] = "@function.outer",
 							["[["] = "@class.outer",
 						},
 						goto_previous_end = {
-							["[M"] = "@function.outer",
+							["[+"] = "@function.outer",
 							["[]"] = "@class.outer",
 						},
 					},
