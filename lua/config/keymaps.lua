6aa8edf997bd886db063cd28d5c66a0d9a5f33c4
Brendan Armstrong <brendanarmstrong@proton.me>
2024-11-24 12:01:09 -0500

    cleaning

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,7 +33,6 @@ vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>")
 -- disable troublesome keys
 vim.keymap.set("n", "<C-z>", "<nop>")
 vim.keymap.set("i", "<C-z>", "<nop>")
--- vim.keymap.set("n", "<C-l>", "<nop>")
 
 -- nice replace macro, works with counts now
 -- "https://github.com/community/community/discussions/29817"
