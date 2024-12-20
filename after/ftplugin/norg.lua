26e09a63ebaf205ef104a8da0fb59644b34ff575
Brendan Armstrong <brendanarmstrong@proton.me>
2024-12-20 08:38:45 -0500

    add autowriteall to norg files

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -1,7 +1,8 @@
 vim.opt.textwidth = 80
 vim.opt.autoindent = false
+vim.o.autowriteall = true
 
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = {"*.norg"},
-  command = "set conceallevel=3"
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.norg" },
+	command = "set conceallevel=3",
 })
